72889e0763ccaae9eba1abb01fd5c2d6c20510aa
wardog85 <chrisja18@gmail.com>
2025-03-06 17:16:57 -0500

    full functioning bookbot

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+
+import sys
 from stats import get_num_words
 
 from stats import get_num_characters
 
+from stats import character_list
+
 def main():
-    words = get_num_words("/home/chris/projects/github.com/wardog85/bookbot/books/frankenstein.txt")
-    characters = get_num_characters("/home/chris/projects/github.com/wardog85/bookbot/books/frankenstein.txt")
-    print(words, characters)
+     # Check if the correct number of arguments were provided
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    # Use the provided filepath instead of the hardcoded one
+    filepath = sys.argv[1]
+ 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")  
+    words = get_num_words(filepath)
+    print("----------- Word Count ----------")
+    print (f"Found {words} total words")
+    print("--------- Character Count -------")
+    characters = get_num_characters(filepath)
+    char_count1 = character_list(filepath)
+    for char in char_count1:
+       print(f"{char['char']}: {char['value']}")
+
+    print("============= END ===============")
+
+
+    
+
 
 main()
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def get_num_words(filepath):
         file_contents = f.read()
         words = file_contents.split()
         num_words = len(words)
-    return f"{num_words} words found in the document"
+    return num_words
 
 
 def get_book_text(filepath):
@@ -23,3 +23,24 @@ def get_num_characters(filepath):
             else:
                 character_dict[i] = 1
     return character_dict
+
+def sort_on(dict):
+    return dict['value']
+
+def character_list(filepath):
+    character_dict = {}
+    with open(filepath) as f:
+        file_contents = f.read()
+        lowercase_contents = file_contents.lower()
+        for i in lowercase_contents:
+            if i in character_dict:
+                character_dict[i] += 1
+            else:
+                character_dict[i] = 1
+    char_list = []
+    for key in character_dict:
+        if key.isalpha():
+            char_list.append({ 'char': key, 'value': character_dict[key] })
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
